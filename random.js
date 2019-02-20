57759f72ff6a51b07aa0f2847d6bc5de88825b2f
LYC <lyc242@qq.com>
2019-02-21 03:07:38 +0800

    Update random.js

--- random.js
+++ random.js
@@ -1,5 +1,17 @@
+/*
+The Random Number Guessing Game
+Generate a number between 1 and 6
+gives a player two attemtps to guess
+*/
+
+//assume the player didnt guess correctly
 var correctGuess = false;
+
+
+//gen number 
 var ranNumber = Math.floor(Math.random()*6)+1;
+
+//test to see if the guess is correct
 var guess = prompt("I am thinking of a number between 1 and 6. What is it?");
 if (parseInt(guess) === ranNumber) {
     correctGuess = true;
@@ -15,6 +27,8 @@ if (parseInt(guess) === ranNumber) {
     }
 }  
 
+
+//output
 if (correctGuess) {
     document.write("<p>You guessed the Number!</p>");
 } else {
